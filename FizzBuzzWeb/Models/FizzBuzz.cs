4c08a3df9f05a5199b18ac84efcf552c095dad58
waleery <walenda1997@gmail.com>
2022-03-20 19:12:57 +0100

    added method onPost

--- FizzBuzzWeb/Models/FizzBuzz.cs
+++ FizzBuzzWeb/Models/FizzBuzz.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 namespace FizzBuzzWeb.Models
+
 {
     public class FizzBuzz
     {
+        [Display(Name = "Twój szczęsliwy numerek")]
         public int Number { get; set; }
     }
 }
