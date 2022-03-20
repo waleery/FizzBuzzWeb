1f1bb6bea79677a769099670cfb1320471e56058
waleery <walenda1997@gmail.com>
2022-03-20 19:17:32 +0100

    added validation input number

--- FizzBuzzWeb/Models/FizzBuzz.cs
+++ FizzBuzzWeb/Models/FizzBuzz.cs
@@ -6,6 +6,8 @@ namespace FizzBuzzWeb.Models
     public class FizzBuzz
     {
         [Display(Name = "Twój szczęsliwy numerek")]
+        [Required(ErrorMessage = "Wymagane!"), Range(1, 1000, ErrorMessage = "Oczekiwana wartość {0} z zakresu {1} i {2}")]
+
         public int Number { get; set; }
     }
 }
