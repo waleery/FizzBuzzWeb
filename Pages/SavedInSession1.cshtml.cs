c13800e27fa08f545dd25a5bfa3630c6c5266be0
waleery <walenda1997@gmail.com>
2022-03-22 17:36:58 +0100

    addSavedInSession and turn on Session service

---
+++ Pages/SavedInSession1.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FizzBuzzWeb.Pages
+{
+    public class SavedInSession1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
